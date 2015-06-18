736376085378d738f50ed34792a685367e4e63da
rotsee <leo_wallentin@hotmail.com>
2015-06-18 11:40:23 +0200

    listen for other ports

--- index.php
+++ index.php
@@ -5,15 +5,31 @@
 <ul>
   <?php
 
+  $host = 'localhost';
   $ignore = array( 'html' );
+  $ports = array(3000, 3001, 3030, 3031, 8080, 8081);
 
   $dirs = glob( dirname( __FILE__ ) . "/*", GLOB_ONLYDIR );
 
+  /* List all subdirs */
   foreach( $dirs as $dir ){
     $dir = basename( $dir );
     if( !in_array( $dir, $ignore ) ){
       echo "<li><a href=\"$dir\">$dir</a>";
     }
+  }
+
+  /* Also check commons ports */
+  foreach ($ports as $port)
+  {
+      $connection = @fsockopen($host, $port);
+
+      if (is_resource($connection))
+      {
+          echo "<li><a href=\"http://$host:$port\">$host:$port</a>";
+          fclose($connection);
+      }
+
   }
   
   ?>
