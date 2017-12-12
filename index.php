cc05b7d31a5ea4d5676ef6de467e6a00ad87ad86
Leo Wallentin <mejl@leowallentin.se>
2017-12-12 13:46:33 +0100

    more ports

--- index.php
+++ index.php
@@ -8,28 +8,34 @@
   $host = 'localhost';
   $ignore = array( 'html' );
   /* Ports to scan for available services */
-  $ports = array(81, 1433, 1434, 1521, 1522, 1525, 1529, 3000, 3001, 3030, 3031, 3306, 3333, 5000, 5001, 5050, 5800, 5900, 8000, 8001, 8080, 8081, 8090, 8888, 9000, 9001);
+  /* https://github.com/search?q=%22app.listen%22&type=Code&utf8=%E2%9C%93 */
+  $ports = array(80, 81,
+                 1337, 1433, 1434, 1521, 1522, 1525, 1529, 1599,
+                 3000, 3001, 3030, 3031, 3300, 3333, 3339, 3999,
+                 4000, 4001, 4873,
+                 5000, 5001, 5050, 5555, 5800, 5900, 5999, 6000,
+                 8000, 8001, 8080, 8081, 8088, 8090, 8091, 8099,
+                 8100, 8101, 8181, 8880, 8881, 8888, 8890, 8891,
+                 9000, 9001, 9900, 9901, 9980, 9981, 9900);
 
   $dirs = glob( dirname( __FILE__ ) . "/*", GLOB_ONLYDIR );
 
   /* List all subdirs */
-  foreach( $dirs as $dir ){
+  foreach($dirs as $dir) {
     $dir = basename( $dir );
-    if( !in_array( $dir, $ignore ) ){
-      echo "<li><a href=\"$dir\">$dir</a>";
+    if (!in_array( $dir, $ignore )) {
+      echo "<li><a href='$dir'>$dir</a>";
     }
   }
 
   /* Also check commons ports */
-  foreach ($ports as $port)
-  {
-      $connection = @fsockopen($host, $port);
+  foreach ($ports as $port) {
+    $connection = @fsockopen($host, $port);
 
-      if (is_resource($connection))
-      {
-          echo "<li><a href=\"http://$host:$port\">$host:$port</a>";
-          fclose($connection);
-      }
+    if (is_resource($connection)) {
+      echo "<li><a href=\"http://$host:$port\">$host:$port</a>";
+      fclose($connection);
+    }
 
   }
   
